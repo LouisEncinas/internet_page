6e013cefe48049c04cd9e9cef506399fd6067d63
Louis Encinas <louis.encinas@etu.uca.fr>
2022-10-03 15:23:07 +0200

    Ajout modal et amélioration

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,10 @@
+let contact_window = document.getElementById('contact_window')
+let contact_button = document.getElementById('contact_button')
+
+contact_button.addEventListener('click', function onOpen() {
+    contact_window.showModal();
+});
+
 $('.header__navbar--toggle').click(function(e) {
     e.preventDefault();
     $('.header__navbar').toggleClass('is_open');
