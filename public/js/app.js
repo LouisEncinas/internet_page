aa625f9fd7e3069679b0c245e081fc34e5dc6328
Louis Encinas <louis.encinas@etu.uca.fr>
2022-10-03 20:55:50 +0200

    V1 fenêtre modale contact

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,25 @@
-let contact_window = document.getElementById('contact_window')
-let contact_button = document.getElementById('contact_button')
+let contact_window = document.getElementById('contact_window');
+let contact_button = document.getElementById('contact_button');
+let close_contact_button = document.getElementById('close_contact_button');
 
-contact_button.addEventListener('click', function onOpen() {
+// Ouvre la modal avec le boutton
+contact_button.addEventListener('click', function() {
     contact_window.showModal();
 });
+
+// Ferme la modal avec le boutton
+close_contact_button.addEventListener('click', function() {
+    contact_window.close();
+});
+
+// Ferme la modal lorsqu'on clique à l'extérieur
+window.onclick = function(event) {
+    if (event.target == contact_window) {
+        contact_window.close();
+    }
+}
 
 $('.header__navbar--toggle').click(function(e) {
     e.preventDefault();
     $('.header__navbar').toggleClass('is_open');
-})
+});
